14705e96e0e6d856895db893b586a5fce9484925
Andrea Dunn Beltran <69739074+asdunnbe@users.noreply.github.com>
2022-11-15 21:03:09 -0500

    update eval (macro as avg)

--- eval.py
+++ eval.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_recall_fscore_support, classification_report
 import csv  
 
 """
@@ -13,49 +13,82 @@ OUTPUT  The function ccreates a csv file with the evaluation metrics seen above.
         TODO: create path correctly
 
 """
+
 def evaluate_predictions(test_name, test_words, hidden_predict):
     hidden_true = [w[1] for w in test_words]
-
-    # seperate into seperate tag classes
-    support_index = {}
-    for i in range(len(hidden_true)):
-        w = hidden_true[i]
-        if w not in support_index.keys():
-            support_index[w] = [i]
-        else:
-            support_index[w].append(i)
+    tags = [lab for lab in set(hidden_true)]
 
     header = ['Tag', 'Accuracy', 'Precision', 'Recall', 'F_score', 'Support']
-
-    # calculate per tags and add to csv
     path = f'{test_name}.csv'
     with open(path, 'w+') as f:
         writer = csv.writer(f)
         writer.writerow(header)
 
-        for sup in support_index:
-            h_true = [hidden_true[i] for i in support_index[sup]]
-            h_pred = [hidden_predict[i] for i in support_index[sup]]
-            TN, FP, FN, TP = confusion_matrix(hidden_true, hidden_predict).ravel()
-            accuracy = (TP+TN)/len(h_true)
-            precision = TP / (TP + FP)
-            recall = TP / (TP + FN)
-            f_score = 2 * ((Precision * Recall) / (Precision + Recall))
+        precisions, recalls, fbeta_scores, supports = precision_recall_fscore_support(hidden_true, hidden_predict, average='macro', labels=tags)
 
-            data = [sup, accuracy, precision, recall, f_score, len(support_index[sup])]
+        for i in range(len(tags)):
+            data = [tags[i], precisions[i], recalls[i], fbeta_scores[i], supports[i]]
             writer.writerow(data)
 
-        # calculate for average and add to csv
-        TN, FP, FN, TP = confusion_matrix(hidden_true, hidden_predict).ravel()
-        Accuracy = (TP+TN)/len(h_true)
-        Precision = TP / (TP + FP)
-        Recall = TP / (TP + FN)
-        F_score = 2 * ((Precision * Recall) / (Precision + Recall))
-
-        data = [test_name, Accuracy, Precision, Recall, F_score, len(hidden_true)]
+        precisions, recalls, fbeta_scores, supports = precision_recall_fscore_support(hidden_true, hidden_predict, average='macro')
+        data = [test_name, precisions[i], recalls[i], fbeta_scores[i], supports[i]]
         writer.writerow(data)
 
-    print(f'The average result for experiment {test_name} are:')
-    print(f'Accuracy: {data[1]}    Precision: {data[2]}    Recall: {data[2]}    F_score: {data[4]}')
+        print(f'The average result for experiment {test_name} are:')
+        print(f'Precision: {data[1]}    Recall: {data[2]}    F_score: {data[3]}')
+
+        print(classification_report(hidden_true, hidden_predict, labels=tags))
 
     return data
+
+
+
+
+
+
+# def evaluate_predictions(test_name, test_words, hidden_predict):
+#     hidden_true = [w[1] for w in test_words]
+
+#     # seperate into seperate tag classes
+#     support_index = {}
+#     for i in range(len(hidden_true)):
+#         w = hidden_true[i]
+#         if w not in support_index.keys():
+#             support_index[w] = [i]
+#         else:
+#             support_index[w].append(i)
+
+#     header = ['Tag', 'Accuracy', 'Precision', 'Recall', 'F_score', 'Support']
+
+#     # calculate per tags and add to csv
+#     path = f'{test_name}.csv'
+#     with open(path, 'w+') as f:
+#         writer = csv.writer(f)
+#         writer.writerow(header)
+
+#         for sup in support_index:
+#             h_true = [hidden_true[i] for i in support_index[sup]]
+#             h_pred = [hidden_predict[i] for i in support_index[sup]]
+#             TN, FP, FN, TP = confusion_matrix(hidden_true, hidden_predict).ravel()
+#             accuracy = (TP+TN)/len(h_true)
+#             precision = TP / (TP + FP)
+#             recall = TP / (TP + FN)
+#             f_score = 2 * ((Precision * Recall) / (Precision + Recall))
+
+#             data = [sup, accuracy, precision, recall, f_score, len(support_index[sup])]
+#             writer.writerow(data)
+
+#         # calculate for average and add to csv
+#         TN, FP, FN, TP = confusion_matrix(hidden_true, hidden_predict).ravel()
+#         Accuracy = (TP+TN)/len(h_true)
+#         Precision = TP / (TP + FP)
+#         Recall = TP / (TP + FN)
+#         F_score = 2 * ((Precision * Recall) / (Precision + Recall))
+
+#         data = [test_name, Accuracy, Precision, Recall, F_score, len(hidden_true)]
+#         writer.writerow(data)
+
+#     print(f'The average result for experiment {test_name} are:')
+#     print(f'Accuracy: {data[1]}    Precision: {data[2]}    Recall: {data[2]}    F_score: {data[4]}')
+
+#     return data
